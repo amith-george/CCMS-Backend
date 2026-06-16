74452308298ddd608ef35f831f8909b51b6e3f74
amith-george <amithgeorge130@gmail.com>
2026-06-16 10:55:50 +0530

    CaseDetailDto Created.

--- src/CCMS.Application/DTOs/CaseDetailsDto.cs
+++ src/CCMS.Application/DTOs/CaseDetailsDto.cs
@@ -4,10 +4,6 @@ namespace CCMS.Application.DTOs
 {
     public class CaseDetailsDto : CaseDto
     {
-        public string? BankRemarks { get; set; }
-        public string? SystemRemarks { get; set; }
-        public string? BatchAccountStatus { get; set; }
-
         public List<CaseDocumentDto> Documents { get; set; } = new List<CaseDocumentDto>();
     }
 }
